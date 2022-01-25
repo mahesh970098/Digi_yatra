80cba3f6565954299a359451eeb1a8456e74cc22
Master <https://github.com/mahesh970098/Digi_yatra>
2022-01-25 15:41:25 +0530

    25-01:3:41

--- app/src/main/java/com/example/digi_yatra_12/fragments/Home_fragment.java
+++ app/src/main/java/com/example/digi_yatra_12/fragments/Home_fragment.java
@@ -1,7 +1,6 @@
 package com.example.digi_yatra_12.fragments;
 
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,43 +11,41 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.digi_yatra_12.MainActivity;
 import com.example.digi_yatra_12.R;
+
+import com.example.digi_yatra_12.databinding.FragmentHomeFragmentBinding;
 import com.example.digi_yatra_12.signup.Signup;
 
 public class Home_fragment extends Fragment {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-    public Button button = null;
-    Intent intent;
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home_fragment, container, false);
-
-
-
-
-        Button btnFragment = (Button) view.findViewById(R.id.example);
-        btnFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Extra_fragment fragment2=new Extra_fragment();
-//                FragmentManager fragmentManager=getActivity().getFragmentManager();
-//                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_con,fragment2, "tag");
-//                fragmentTransaction.addToBackStack(null);
-//                fragmentTransaction.commit();
-//                Intent intent =new Intent(Home_fragment.this, Extra_fragment.class);
-//                startActivity(intent);
-            }
-        });
-        return view;
+FragmentHomeFragmentBinding binding;
+    public Home_fragment() {
+        // Required empty public constructor
     }
 
 
 
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+    binding =FragmentHomeFragmentBinding.inflate(inflater,container,false);
+    binding.example.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Home_fragment2 fragment2 =new Home_fragment2();
+            FragmentTransaction transaction =getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_con,fragment2);
+
+            transaction.commit();
+
+            //            getFragmentManager().beginTransaction().replace(R.id.container,new Home_fragment2()).commit();
+
+
+        }
+    });
+    return binding.getRoot();
+    }
 }
--- app/src/main/java/com/example/digi_yatra_12/fragments/Wallet_fragment.java
+++ app/src/main/java/com/example/digi_yatra_12/fragments/Wallet_fragment.java
@@ -1,17 +1,27 @@
 package com.example.digi_yatra_12.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.digi_yatra_12.R;
 
 public class Wallet_fragment extends Fragment {
+    Button add;
+    Layout add1;
+    Wallet_fragment firstfragment;
+    Home_fragment2 secondFragment;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,48 @@ public class Wallet_fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wallet_fragment, container, false);
-        return view;
+return view;
+
     }
+
+
+
+    public void onViewCreated(View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+         add = (Button) view.findViewById(R.id.addBtn);
+        add.setOnClickListener(this::onClick);
+    }
+
+
+        public void onClick(View v) {
+
+//            if(v.getId()==R.id.home_fragment2){
+//            getFragmentManager().beginTransaction().replace(R.id.container,new Home_fragment2()).commit();
+
+//                Home_fragment2 fragment2 =new Home_fragment2();
+//                FragmentTransaction transaction =getFragmentManager().beginTransaction();
+//                transaction.replace(R.id.walletfragment,fragment2);
+//
+//                transaction.commit();
+//                add.setVisibility(getView().GONE);
+//
+//
+
+            FragmentTransaction fragmentTransaction = getActivity()
+                    .getSupportFragmentManager().beginTransaction();
+//            fragmentTransaction.replace(R.id.walletfragment, new Home_fragment2());
+
+            fragmentTransaction.hide(firstfragment);
+            fragmentTransaction.show(secondFragment);
+
+            fragmentTransaction.commit();
+
+
+
+
+
+
+        }
+
 }
